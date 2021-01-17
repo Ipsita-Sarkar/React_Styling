7e0f2c92d3fec806ccac7cbbc781f7d8b12908d9
sheerangel <77532755+sheerangel@users.noreply.github.com>
2021-01-17 20:08:17 +0530

    Styling Changed

--- src/index.js
+++ src/index.js
@@ -10,11 +10,20 @@ import ReactDOM from "react-dom";
 const date = new Date();
 const currentTime = date.getHours();
 let greet;
+const custStyle = { color: "" };
 if (currentTime < 12) {
   greet = "GOOD MORNING";
+  custStyle.color = "yellow";
 } else if (currentTime < 18) {
   greet = "GOOD AFTERNOON";
+  custStyle.color = "orange";
 } else {
   greet = "GOOD NIGHT";
+  custStyle.color = "blue";
 }
-ReactDOM.render(<h1>{greet}</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <h1 className="heading" style={custStyle}>
+    {greet}
+  </h1>,
+  document.getElementById("root")
+);
